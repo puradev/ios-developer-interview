2c44ae2310629c3a38719f2c289d1200d257b666
Gustavo Colaço <gustavo.colaco@hotmail.com>
2023-03-03 11:33:43 -0300

    Feature: Refactored the ViewController
    Removed the old way that it was showing data on the View Controller, and made it show the SwiftUI View instead.

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -6,53 +6,20 @@
 //
 
 import UIKit
+import SwiftUI
+
 
 class ViewController: UIViewController {
-
-    var dataSource = TableViewDataSource(state: .empty)
     
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var tableView: UITableView!
-    
-    
-    @IBAction func didTapButton() {
-        guard let text = textField.text else {
-            return
-        }
-        
-        API.shared.fetchWord(query: text) { response in
-            switch response {
-            case .success(let data):
-                guard let r = WordResponse.parseData(data) else {
-                    return
-                }
-                
-                self.dataSource.updateState(.word(r.word)) {
-                    self.tableView.reloadData()
-                }
-                
-            case .failure(let error):
-                self.dataSource.updateState(.empty) {
-                    self.tableView.reloadData()
-                }
-                print("NETWORK ERROR: ", error.localizedDescription)
-            }
-        }
-    }
+    let childVC = UIHostingController(rootView: SearchedWordScreen())
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        tableView.dataSource = dataSource
-        tableView.delegate = self
-        
-        tableView.rowHeight = UITableView.automaticDimension
+        addChild(childVC)
+        view.addSubview(childVC.view)
+        childVC.view.frame = view.frame
     }
 
-
 }
 
-extension ViewController: UITableViewDelegate {
-    
-}
