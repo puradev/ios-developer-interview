eaf18ca745d845b33e800fde4aafb159b909e965
Pedro Ullmann <pedroullmann@hotmail.com>
2023-03-06 15:58:36 -0300

    Feat: WordDefinitionStateTests

--- Modules/Tests/Feature-WordDefinition-Tests/WordDefinitionStateTests.swift
+++ Modules/Tests/Feature-WordDefinition-Tests/WordDefinitionStateTests.swift
@@ -1,8 +1,43 @@
-//
-//  File.swift
-//  
-//
-//  Created by Pedro Ullmann on 06/03/23.
-//
+@testable import Feature_WordDefinition
+import XCTest
 
-import Foundation
+final class WordDefinitionStateTests: XCTestCase {
+    func test_isSearchDisabled_whenQueryCountIsLessThan2_shouldReturnTrue() {
+        // Given
+        let sut = WordDefinitionState(query: "a")
+
+        // When
+        let isSearchDisabled = sut.isSearchDisabled
+
+        // Then
+        XCTAssert(isSearchDisabled)
+    }
+
+    func test_isSearchDisabled_whenDataStateIsLoading_shouldReturnTrue() {
+        // Given
+        let sut = WordDefinitionState(
+            dataState: .loading,
+            query: "apple"
+        )
+
+        // When
+        let isSearchDisabled = sut.isSearchDisabled
+
+        // Then
+        XCTAssert(isSearchDisabled)
+    }
+
+    func test_isSearchDisabled_whenDataStateIsNotLoading_andQueryIsGreaterThan2_shouldReturnFalse() {
+        // Given
+        let sut = WordDefinitionState(
+            dataState: .idle,
+            query: "apple"
+        )
+
+        // When
+        let isSearchDisabled = sut.isSearchDisabled
+
+        // Then
+        XCTAssertFalse(isSearchDisabled)
+    }
+}
