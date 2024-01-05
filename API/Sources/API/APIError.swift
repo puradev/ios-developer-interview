486b404a5f2ce1271ad74f12346327cc071a8302
Nathan Fulkerson <NFulkerson@users.noreply.github.com>
2024-01-05 10:43:21 -0500

    Give errors more meaningful description

--- API/Sources/API/APIError.swift
+++ API/Sources/API/APIError.swift
@@ -7,10 +7,25 @@
 
 import Foundation
 
-public enum APIError: Error {
+public enum APIError: Error, Equatable {
     case badURL
     case custom(String)
     case noData
     case emptyQuery
     case tooShort(String)
+
+  var localizedDescription: String {
+    switch self {
+    case .badURL:
+      return "Bad URL request. Check your query and try again."
+    case .custom(let string):
+      return string
+    case .noData:
+      return "No data was returned. Try again."
+    case .emptyQuery:
+      return "Your query was empty. Enter a word and try again."
+    case .tooShort(_):
+      return "Your query was too short. Try again."
+    }
+  }
 }
