7235511cbb26cb898b9e92c5c6a3e03ac4a5d14e
Nathan Fulkerson <NFulkerson@users.noreply.github.com>
2024-01-05 09:54:57 -0500

    Retrieve headword and functional label from response

--- SampleApp/Model/WordResponse.swift
+++ SampleApp/Model/WordResponse.swift
@@ -10,22 +10,23 @@ import Foundation
 struct WordResponse: Codable {
     let meta: Meta
     let shortdef: [String]
+    let hwi: HeadwordInfo
+    let fl: String
+
+    struct HeadwordInfo: Codable {
+        let hw: String
+    }
 
     var word: Word {
-        return Word(text: meta.stems.first!, definitions: shortdef)
+        return Word(id: meta.id, text: hwi.hw, definitions: shortdef, functionalLabel: fl)
     }
 
-    static func words(from data: Data) -> [WordResponse]? {
-        do {
-            let response = try JSONDecoder().decode([WordResponse].self, from: data)
-            return response
-        } catch {
-            print("WORD RESPONSE ERROR: ", error.localizedDescription)
-        }
-        return nil
+    static func words(from data: Data) throws -> [WordResponse]? {
+        let response = try JSONDecoder().decode([WordResponse].self, from: data)
+        return response
     }
 
-    static func word(from data: Data) -> WordResponse? {
-      words(from: data)?.first
+    static func word(from data: Data) throws -> WordResponse? {
+        try words(from: data)?.first
     }
 }
