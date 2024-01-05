eee29fdb8fc9cf7c5595eeac51f6306486e5cf3f
Nathan Fulkerson <NFulkerson@users.noreply.github.com>
2024-01-04 19:50:28 -0500

    Convert in full to SwiftUI

--- SampleApp/SwiftUI/SearchView.swift
+++ SampleApp/SwiftUI/SearchView.swift
@@ -6,13 +6,70 @@
 //
 
 import SwiftUI
+import API
+
+@Observable
+class SearchState {
+  var error: APIError?
+  var words: [Word] = []
+  var query: String = ""
+
+  func searchTapped() async {
+    let response = await API.shared.fetchWord(query: query)
+
+    switch response {
+    case .success(let data):
+        guard let response = WordResponse.words(from: data) else {
+            return
+        }
+
+      self.words = response.map { $0.word }
+
+    case .failure(let error):
+      self.words = []
+      print("NETWORK ERROR: ", error.localizedDescription)
+    }
+  }
+}
 
 struct SearchView: View {
+  @State var state: SearchState
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      List {
+        ForEach(state.words, id: \.text) { word in
+          VStack(alignment: .leading) {
+            Text(word.text)
+            Text(word.definitions.joined(separator: ","))
+          }
+        }
+      }
+      .listStyle(.plain)
+      .safeAreaInset(edge: .top) {
+        HStack {
+          TextField(
+            "Search",
+            text: $state.query,
+            prompt: Text("Search for a word")
+          )
+          .padding(8)
+          .background(
+            Color.init(hue: 0, saturation: 0, brightness: 0.95)
+            .clipShape(.rect(cornerRadius: 8))
+          )
+
+          Button("Search") {
+            Task {
+              await state.searchTapped()
+            }
+          }
+          .buttonStyle(.borderedProminent)
+        }
+        .padding()
+      }
     }
 }
 
 #Preview {
-    SearchView()
+  SearchView(state: SearchState())
 }
