f5db83ca4f9f6b9feca5fdf1219be4cc517f962c
Mark Davis <mark.j.davis2@gmail.com>
2024-01-20 13:01:43 -0500

    convert storyboard to swiftui layout

--- SampleApp/SwiftUI/ContentView.swift
+++ SampleApp/SwiftUI/ContentView.swift
@@ -7,23 +7,53 @@
 
 import SwiftUI
 
-struct ContentView: UIViewControllerRepresentable {
-    typealias UIViewControllerType = ViewController
-
-    func makeUIViewController(context: Context) -> ViewController {
-        
-        // Load Main.storyboard
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        
-        // Instantiate ViewController
-        guard let viewController = storyboard.instantiateInitialViewController() as? ViewController else {
-            fatalError("Couldn't instanciate a ViewController class.")
-        }
-        
-        return viewController
-    }
+struct ContentView: View {
+    @State private var userInput: String = ""
+    @State private var wordDefinitions: [String : String] = [:]
     
-    func updateUIViewController(_ uiViewController: ViewController, context: Context) {
-        
+    var body: some View {
+        VStack(alignment: .leading) {
+            // Header
+            VStack(alignment: .leading, spacing: 15) {
+                Text("Dictionary")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                
+                
+                Text("Enter a word below, and see the definition!")
+            } //: VStack
+            .padding(.bottom, 40)
+            
+            HStack {
+                // Text Entry
+                TextField("", text: $userInput, prompt: Text("Enter here"))
+                    .textFieldStyle(.roundedBorder)
+                    .autocorrectionDisabled()
+                
+                // Submit Button
+                Button("Submit") {
+                    print(userInput)
+                }
+                .buttonStyle(.borderedProminent)
+            }
+
+            // Results List
+            List {
+                ForEach(Array(wordDefinitions.keys), id: \.self) { word in
+                    VStack(alignment: .leading) {
+                        Text(word)
+                        Text(wordDefinitions[word] ?? "")
+                    }
+                }
+            }
+            
+        } //: VStack
+        .padding(15)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
