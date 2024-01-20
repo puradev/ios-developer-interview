1a72514ed49e4578582c2d909d18509358d45202
Mark Davis <mark.j.davis2@gmail.com>
2024-01-20 14:45:00 -0500

    add in service call

--- SampleApp/SwiftUI/ContentView.swift
+++ SampleApp/SwiftUI/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var userInput: String = ""
     @State private var wordDefinitions: [String : String] = [:]
+    @State private var isApiCallInProgress: Bool = false
+    @State private var presentEmptyDefinitionAlert: Bool = false
+    @State private var presentApiFailureAlert: Bool = false
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -32,9 +35,12 @@ struct ContentView: View {
                 
                 // Submit Button
                 Button("Submit") {
+                    isApiCallInProgress = true
                     print(userInput)
+                    fetchWordDefinition(with: userInput)
                 }
                 .buttonStyle(.borderedProminent)
+                .disabled(isApiCallInProgress)
             }
 
             // Results List
@@ -42,6 +48,8 @@ struct ContentView: View {
                 ForEach(Array(wordDefinitions.keys), id: \.self) { word in
                     VStack(alignment: .leading) {
                         Text(word)
+                            .fontWeight(.heavy)
+                            .padding(.bottom, 5)
                         Text(wordDefinitions[word] ?? "")
                     }
                 }
@@ -49,8 +57,42 @@ struct ContentView: View {
             
         } //: VStack
         .padding(15)
+        .alert("No definition found, please try again", isPresented: $presentEmptyDefinitionAlert) {
+            Button("Ok", action: {
+                userInput = ""
+            })
+        }
+        .alert("API failure, please try again", isPresented: $presentApiFailureAlert) {
+            Button("Ok", action: {})
+        }
+    }
+    
+    func fetchWordDefinition(with word: String) {
+        API.shared.fetchWord(query: userInput) { response in
+            switch response {
+            case .success(let data):
+                guard let r = WordResponse.parseData(data) else {
+                    presentEmptyDefinitionAlert = true
+                    return
+                }
+                
+                let definition = r.shortdef[0]
+                guard !definition.isEmpty else {
+                    presentEmptyDefinitionAlert = true
+                    return
+                }
+                self.wordDefinitions[userInput] = definition
+                
+            case .failure(let error):
+                presentApiFailureAlert = true
+                print("NETWORK ERROR: ", error.localizedDescription)
+            }
+            isApiCallInProgress = false
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
