c2c0fa30e6f4e4b51d4a087415912e7796e139ba
Jonathan Davis <jonathan@obj.co>
2024-08-25 04:20:08 -0600

    Refactor to AppDelegate and SceneDelegate to SwiftUI App

--- Etymo/ViewController.swift
+++ Etymo/ViewController.swift
@@ -6,31 +6,30 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
     var dataSource = TableViewDataSource(state: .empty)
-    
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var tableView: UITableView!
-    
-    
+
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var tableView: UITableView!
+
     @IBAction func didTapButton() {
         guard let text = textField.text else {
             return
         }
-        
+
         API.shared.fetchWord(query: text) { response in
             switch response {
             case .success(let data):
                 guard let r = WordResponse.parseData(data) else {
                     return
                 }
-                
+
                 self.dataSource.updateState(.word(r.word)) {
                     self.tableView.reloadData()
                 }
-                
+
             case .failure(let error):
                 self.dataSource.updateState(.empty) {
                     self.tableView.reloadData()
@@ -39,20 +38,30 @@ class ViewController: UIViewController {
             }
         }
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+
         tableView.dataSource = dataSource
         tableView.delegate = self
-        
+
         tableView.rowHeight = UITableView.automaticDimension
     }
-
-
 }
 
-extension ViewController: UITableViewDelegate {
-    
+extension ViewController: UITableViewDelegate {}
+
+struct ViewControllerView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ViewController
+
+    func makeUIViewController(context: Context) -> ViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+        let viewController = storyboard.instantiateInitialViewController() as! ViewController
+
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: ViewController, context: Context) {}
 }
