17c143fa5c65f20b1aeca5dee15281221ce6db2b
Jonathan Davis <jonathan@obj.co>
2024-08-25 05:07:26 -0600

    Refactors ViewController to SwiftUI

--- Etymo/ContentView.swift
+++ Etymo/ContentView.swift
@@ -1,8 +1,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = ""
+    @State private var word: Word?
+
     var body: some View {
-        ViewControllerView()
+        NavigationView {
+            ScrollView {
+                if let word {
+                    VStack {
+                        Text("\(word.text)")
+                        ForEach(Array(word.definitions.enumerated()), id: \.offset) { index, definition in
+                            Text("\(index + 1). \(definition)")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Etymo")
+            .searchable(text: $searchText, prompt: "Word")
+            .onChange(of: searchText) { text in
+                API.shared.fetchWord(query: text) { response in
+                    switch response {
+                    case .success(let data):
+                        guard let r = WordResponse.parseData(data) else {
+                            return
+                        }
+
+                        word = r.word
+                    case .failure(let error):
+                        word = nil
+                        print("NETWORK ERROR: ", error.localizedDescription)
+                    }
+                }
+            }
+        }
     }
 }
 
