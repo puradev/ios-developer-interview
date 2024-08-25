3fd6382cab7714ee303ffc07c59333ccca175609
Jonathan Davis <jonathan@obj.co>
2024-08-25 05:25:10 -0600

    Adds no results view

--- Etymo/ContentView.swift
+++ Etymo/ContentView.swift
@@ -6,34 +6,62 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-            ScrollView {
-                if let word {
+            GeometryReader { geometry in
+                ScrollView {
                     VStack {
-                        Text("\(word.text)")
-                        ForEach(Array(word.definitions.enumerated()), id: \.offset) { index, definition in
-                            Text("\(index + 1). \(definition)")
+                        if let word {
+                            Text("\(word.text)")
+                            ForEach(Array(word.definitions.enumerated()), id: \.offset) { index, definition in
+                                Text("\(index + 1). \(definition)")
+                            }
+                        } else {
+                            noResultsView
                         }
                     }
+                    .frame(width: geometry.size.width)
+                    .frame(minHeight: geometry.size.height)
                 }
-            }
-            .navigationTitle("Etymo")
-            .searchable(text: $searchText, prompt: "Word")
-            .onChange(of: searchText) { text in
-                API.shared.fetchWord(query: text) { response in
-                    switch response {
-                    case .success(let data):
-                        guard let r = WordResponse.parseData(data) else {
-                            return
+                
+                .navigationTitle("Etymo")
+                .searchable(text: $searchText, prompt: "Word")
+                .onChange(of: searchText) { text in
+                    API.shared.fetchWord(query: text) { response in
+                        switch response {
+                        case .success(let data):
+                            guard let r = WordResponse.parseData(data) else {
+                                return
+                            }
+                            
+                            word = r.word
+                        case .failure(let error):
+                            word = nil
+                            print("NETWORK ERROR: ", error.localizedDescription)
                         }
-
-                        word = r.word
-                    case .failure(let error):
-                        word = nil
-                        print("NETWORK ERROR: ", error.localizedDescription)
                     }
                 }
             }
         }
+    }
+    
+    var noResultsView: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "text.page.badge.magnifyingglass")
+                .font(.largeTitle.weight(.bold))
+                .imageScale(.large)
+                .foregroundStyle(.secondary)
+                .accessibilityHidden(true)
+            
+            VStack(spacing: 4) {
+                Text("No Results")
+                    .font(.headline)
+                Text("Check the spelling or try a new search.")
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+            }
+            .accessibilityElement(children: .combine)
+        }
+        .padding(.bottom, 48)
+        .frame(maxHeight: .infinity, alignment: .center)
     }
 }
 
