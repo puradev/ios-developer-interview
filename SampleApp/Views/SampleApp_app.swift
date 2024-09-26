b063682ae2aae7e557d02594429fea38975ff49d
Ben Patch <fluffypatch@gmail.com>
2024-09-26 15:23:36 -0600

    Clear out UIKit 🔥🔥

---
+++ SampleApp/Views/SampleApp_app.swift
@@ -0,0 +1,19 @@
+//
+//  SampleApp_app.swift
+//  SampleApp
+//
+//  Created by Benjamin Patch on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct AppView: App {
+    var body: some Scene {
+        
+        WindowGroup {
+            RootView()
+        }
+    }
+}
+
